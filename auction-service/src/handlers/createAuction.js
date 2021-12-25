acda15fc4af2991fe53460e6f9d004c27eed9478
Zishuo Xu <xuzishuo1996@gmail.com>
2021-12-25 16:50:12 -0500

    add create auction lambda function

---
+++ auction-service/src/handlers/createAuction.js
@@ -0,0 +1,19 @@
+async function createAuction(event, context) {
+  const { title } = JSON.parse(event.body);
+  const now = new Date();
+
+  const auction = {
+    title,
+    status: 'OPEN',
+    createAt: now.toISOString(),
+  };
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({auction}),
+  };
+}
+
+export const handler = createAuction;
+
+
