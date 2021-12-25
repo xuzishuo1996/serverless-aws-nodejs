3c3450a8fa56cd40dcb50d9224d10490052ce9d5
Zishuo Xu <xuzishuo1996@gmail.com>
2021-12-25 17:22:20 -0500

    add entry to DynamoDB

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,12 +1,26 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
   const { title } = JSON.parse(event.body);
   const now = new Date();
 
   const auction = {
+    id: uuid(),
     title,
     status: 'OPEN',
     createAt: now.toISOString(),
   };
+
+  // ref: https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html
+  // await and promise are the JS ES6 properties
+  await dynamodb.put({
+    // first letters of properties have to be upper letters
+    TableName: 'AuctionsTable',
+    Item: auction,
+  }).promise();
 
   return {
     statusCode: 201,
