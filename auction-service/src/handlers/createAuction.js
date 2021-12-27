8f8d5d44b48754ae07226ce8ed2e3093dcced46f
Zishuo Xu <xuzishuo1996@gmail.com>
2021-12-27 18:16:17 -0500

    specify seller identity from the email field of the decoded JWT token

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -9,6 +9,7 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
   const { title } = event.body; // httpJsonBodyParser() does JSON.parse(event.body)
+  const { email } = event.requestContext.authorizer;
   const now = new Date();
   const endDate = new Date();
   endDate.setHours(now.getHours() + 1);
@@ -22,6 +23,7 @@ async function createAuction(event, context) {
     highestBid: {
       amount: 0,
     },
+    seller: email,
   };
 
   try {
