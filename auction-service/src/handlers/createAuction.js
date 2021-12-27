58532feacde6219b8fd5a28a62bdbe5625402690
Zishuo Xu <xuzishuo1996@gmail.com>
2021-12-27 11:17:10 -0500

    use some middlewares

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,10 +1,15 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
+import middy from '@middy/core';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
-  const { title } = JSON.parse(event.body);
+  const { title } = event.body; // httpJsonBodyParser() does JSON.parse(event.body)
   const now = new Date();
 
   const auction = {
@@ -14,14 +19,20 @@ async function createAuction(event, context) {
     createAt: now.toISOString(),
   };
 
-  // ref: https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html
-  // await and promise are the JS ES6 properties
-  await dynamodb.put({
-    // first letters of properties have to be upper letters
-    // process.env is how to get env variables in JS
-    TableName: process.env.AUCTIONS_TABLE_NAME,
-    Item: auction,
-  }).promise();
+  try {
+    // ref: https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html
+    // await and promise are the JS ES6 properties
+    await dynamodb.put({
+      // first letters of properties have to be upper letters
+      // process.env is how to get env variables in JS
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction,
+    }).promise();
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
 
   return {
     statusCode: 201,
@@ -29,6 +40,7 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = createAuction;
-
-
+export const handler = middy(createAuction)
+.use(httpJsonBodyParser())
+.use(httpEventNormalizer())
+.use(httpErrorHandler());
