e667c9f1806a0f7212080546ebb8c157572e88d6
Zishuo Xu <xuzishuo1996@gmail.com>
2021-12-27 16:19:05 -0500

    add json schema validation for getAuctions

--- auction-service/src/handlers/getAuctions.js
+++ auction-service/src/handlers/getAuctions.js
@@ -1,6 +1,10 @@
 import AWS from 'aws-sdk';
 import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
+// for validating json schema
+import validator from '@middy/validator';
+import getAuctionsSchema from '../lib/schemas/getAuctionsSchema';
+
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -39,4 +43,13 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = commonMiddleware(getAuctions);
+export const handler = commonMiddleware(getAuctions)
+	.use(
+		validator({
+			inputSchema: getAuctionsSchema,
+			ajvOptions: {
+				useDefaults: true,
+				strict: false,
+			},
+		})
+	);
