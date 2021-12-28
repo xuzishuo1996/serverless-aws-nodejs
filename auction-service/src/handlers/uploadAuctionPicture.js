d516ccc0a304d505a4e5d2c5defa0e2378e27a6f
Zishuo Xu <xuzishuo1996@gmail.com>
2021-12-28 15:47:25 -0500

    fix white pic problem. Buffer.from(pic, 'base64')

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -2,19 +2,18 @@ import { getAuctionById } from './getAuction';
 import { uploadPictureToS3 } from '../lib/uploadPictureToS3';
 
 export async function uploadAuctionPicture(event) {
-    const { id } = event.pathParameters;
+  const { id } = event.pathParameters;
+  const auction = await getAuctionById(id);
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
+  const buffer = Buffer.from(base64, 'base64');
 
-    const auction = await getAuctionById(id);
-    const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
-    const buffer = Buffer.from(base64);
+  const uploadToS3Result = await uploadPictureToS3(auction.id + '.jpg', buffer);
+  console.log(uploadToS3Result);
 
-    const uploadToS3Result = await uploadPictureToS3(auction.id + '.jpg', buffer);
-    console.log(uploadToS3Result);
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify({}),
-    };
+  return {
+    statusCode: 200,
+    body: JSON.stringify({}),
+  };
 }
 
 export const handler = uploadAuctionPicture;
