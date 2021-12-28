b105d46a58b6a6e84f14e02bcf5caa571c05b58a
Zishuo Xu <xuzishuo1996@gmail.com>
2021-12-27 23:12:39 -0500

    send first test email

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -1,8 +1,32 @@
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES({ region: 'us-east-2' });
+
 async function sendMail(event, context) {
-  console.log(event);
-  return event;
+  const params = {
+    Source: 'xuzishuo1996@126.com',
+    Destination: {
+      ToAddresses: ['xuzishuo1996@126.com'],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: 'Hello from Codingly!',
+        },
+      },
+      Subject: {
+        Data: 'Test Mail',
+      },
+    },
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export const handler = sendMail;
-
-
